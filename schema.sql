4ad4ad73cde12aba0ff30b13a14ba08d44cd6522
KLIV <wclivingston5@gmail.com>
2018-08-25 02:49:09 -0500

    not null update

--- schema.sql
+++ schema.sql
@@ -5,10 +5,10 @@ USE bamazonDB;
 
 CREATE TABLE products (
   item_id INT NOT NULL AUTO_INCREMENT,
-  product_name VARCHAR(45) NULL,
-  department_name VARCHAR(45) NULL,
-  price DECIMAL(10,2) NULL,
-  quantity INT NULL,
+  product_name VARCHAR(45) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  quantity INT NOT NULL,
   PRIMARY KEY (item_id)
 );
 
