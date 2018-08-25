5abcadf8d44d357ab0306b0bba609edc77a34410
KLIV <wclivingston5@gmail.com>
2018-08-25 02:46:31 -0500

    overhaul customer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,7 +2,8 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var divider =
-    "\n---------------------------------------------";
+    "\n-----------------------------------------------------";
+var currentInventory = [];
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -17,55 +18,93 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + divider);
-    showItems();
+    console.log("\n" + "Welcome to Bamazon!" + "\n"
+        + "You're connected as id " + connection.threadId + divider);
+    homePage();
 });
+
+function homePage() {
+    inquirer
+        .prompt([{
+            name: 'shop',
+            type: 'confirm',
+            message: 'Would you like to make a purchase?'
+        }
+        ]).then(function (answer) {
+            if (answer.shop) {
+                showItems();
+            } else {
+                console.log('Have a nice day!');
+            }
+        }).catch(function (error) {
+            throw error;
+        });
+}
 
 function showItems() {
 
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        console.log("item_id" + "  |  " + "Product" + "  -  " + "Department" + "  -  " + "Price" + "  -  " + "Qty" + "\n");
+        console.log(divider + "\n" + "item_id" + "  |  " + "Product" + "  -  " + "Department" + "  -  " +
+            "Price" + "  -  " + "Qty" + divider);
+
         for (var i = 0; i < res.length; i++) {
-            console.log(res[i].item_id + " | " + res[i].product_name + " - " + 
-            res[i].department_name + " - " + res[i].price + "Total Avg - " + res[i].quantity + "\n");
+            console.log(res[i].item_id + " | " + res[i].product_name + " - " +
+                res[i].department_name + " - " + res[i].price + " - " + res[i].quantity + "\n");
         }
-        selectID();
-    });
 
+        currentInventory = [];
+        for (var i = 0; i < res.length; i++) {
+            currentInventory.push(res[i].item_id);
+        }
+        shopID();
+    });
 }
 
-function selectID(){
+function shopID() {
     inquirer
-    .prompt({
-      name: "itemID",
-      type: "input",
-      message: "What is the ID of the product you'd like to buy?"
-    })
-    .then(function(answer) {
-      var query = "SELECT * FROM products";
-      connection.query(query, [answer.itemID], function (err, res) {
-        
-        console.log("You've selected " + res.itemID);
-        // selectQty();
-      });
-    });
-}
+        .prompt([{
+            name: 'id',
+            type: 'prompt',
+            message: 'What is the ID of the product you would like to buy?'
+        },
+        {
+            name: 'qty',
+            type: 'prompt',
+            message: 'How many units would you like?'
+        }
+        ]).then(function (answers) {
+            itemID = answers.id;
+            itemQTY = answers.qty;
+            console.log("You've selected " + itemID);
+            if (currentInventory.indexOf(parseInt(itemID)) === -1) {
+                console.log("Sorry, that item is SOLD OUT");
+                homePage();
+            } else {
+                var query = "SELECT * FROM products WHERE ?";
+                connection.query(query, { item_id: answers.itemID }, function (err, res) {
+                    if (err) throw err;
+                    var itemName = res.product_name;
+                    // var currentStock = res[0].quantity;
+                    // var updatedStock = parseInt(res[0].quantity) - itemQTY;
+                    // var totalCost = itemQTY * parseFloat(res[0].price);
+                    console.log(itemName);
+                    // console.log(itemName + "  |  " + currentStock + "  |  " + updatedStock + "  |  " + totalCost);
 
-// function selectQty(){
-//     inquirer
-//     .prompt({
-//       name: "itemID",
-//       type: "input",
-//       message: "What is the ID of the product you'd like to buy?"
-//     })
-//     .then(function(answer) {
-//       var query = "SELECT * FROM products";
-//       connection.query(query, [], function (err, res) {
-//         for (var i = 0; i < res.length; i++) {
-//           console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
-//         }
-//         showItems();
-//       });
-//     });
-// }
+                    // if (currentStock >= itemQTY) {
+
+                    //     connection.query("UPDATE inventory SET ? WHERE ?", [{ item_id: itemID }, { quantity: newStock }], function (err, res) {
+
+                    //         console.log("For" + itemQTY + " units of " + itemName)
+                    //         console.log("You have been charged: $" + totalCost);
+                    //         console.log("Thank you for your purchase!");
+                    //     });
+                    //     connection.end();
+                    // } else {
+                    //     console.log("There is not enough inventory for that purchase");
+                    //     homePage();
+                    // }
+                });
+            }
+        })
+}
