5d10d6bbf07f0316eba8a83bd8a9d24994fe64a4
KLIV <wclivingston5@gmail.com>
2018-08-25 03:00:04 -0500

    need to troubleshoot shop query res

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -77,34 +77,34 @@ function shopID() {
             itemID = answers.id;
             itemQTY = answers.qty;
             console.log("You've selected " + itemID);
-            if (currentInventory.indexOf(parseInt(itemID)) === -1) {
-                console.log("Sorry, that item is SOLD OUT");
-                homePage();
-            } else {
+            if (currentInventory.indexOf(parseInt(itemID)) >= -1) {
                 var query = "SELECT * FROM products WHERE ?";
                 connection.query(query, { item_id: answers.itemID }, function (err, res) {
                     if (err) throw err;
                     var itemName = res.product_name;
-                    // var currentStock = res[0].quantity;
-                    // var updatedStock = parseInt(res[0].quantity) - itemQTY;
-                    // var totalCost = itemQTY * parseFloat(res[0].price);
+                    var currentStock = res.quantity;
+                    var updatedStock = parseInt(res.quantity) - itemQTY;
+                    var totalCost = itemQTY * parseFloat(res.price);
                     console.log(itemName);
-                    // console.log(itemName + "  |  " + currentStock + "  |  " + updatedStock + "  |  " + totalCost);
+                    console.log(itemName + "  |  " + currentStock + "  |  " + updatedStock + "  |  " + totalCost);
 
-                    // if (currentStock >= itemQTY) {
+                    if (currentStock >= itemQTY) {
 
-                    //     connection.query("UPDATE inventory SET ? WHERE ?", [{ item_id: itemID }, { quantity: newStock }], function (err, res) {
+                        connection.query("UPDATE inventory SET ? WHERE ?", [{ item_id: itemID }, { quantity: newStock }], function (err, res) {
 
-                    //         console.log("For" + itemQTY + " units of " + itemName)
-                    //         console.log("You have been charged: $" + totalCost);
-                    //         console.log("Thank you for your purchase!");
-                    //     });
-                    //     connection.end();
-                    // } else {
-                    //     console.log("There is not enough inventory for that purchase");
-                    //     homePage();
-                    // }
-                });
+                            console.log("For" + itemQTY + " units of " + itemName)
+                            console.log("You have been charged: $" + totalCost);
+                            console.log("Thank you for your purchase!");
+                        });
+                        connection.end();
+                    } else {
+                        console.log("There is not enough inventory for that purchase");
+                        homePage();
+                    }
+                })
+            } else {
+                console.log("Sorry, that item is SOLD OUT");
+                homePage();
             }
         })
 }
