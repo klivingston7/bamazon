a2f8c9c0338ad9c455d836f4a07082dfe9cfbf03
KLIV <wclivingston5@gmail.com>
2018-08-25 13:01:04 -0500

    fixed shopID syntax error

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,7 +3,6 @@ var inquirer = require("inquirer");
 
 var divider =
     "\n-----------------------------------------------------";
-var currentInventory = [];
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -20,7 +19,7 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log("\n" + "Welcome to Bamazon!" + "\n"
         + "You're connected as id " + connection.threadId + divider);
-    homePage();
+    showItems();
 });
 
 function homePage() {
@@ -32,7 +31,7 @@ function homePage() {
         }
         ]).then(function (answer) {
             if (answer.shop) {
-                showItems();
+                shopID();
             } else {
                 console.log('Have a nice day!');
             }
@@ -57,7 +56,7 @@ function showItems() {
         for (var i = 0; i < res.length; i++) {
             currentInventory.push(res[i].item_id);
         }
-        shopID();
+        homePage();
     });
 }
 
@@ -73,38 +72,41 @@ function shopID() {
             type: 'prompt',
             message: 'How many units would you like?'
         }
-        ]).then(function (answers) {
-            itemID = answers.id;
-            itemQTY = answers.qty;
+        ]).then(function (data) {
+            itemID = data.id;
+            itemQTY = parseInt(data.qty);
+            var itemName = 0;
+            var currentStock = 0;
+            var itemPrice = 0;
             console.log("You've selected " + itemID);
-            if (currentInventory.indexOf(parseInt(itemID)) >= -1) {
-                var query = "SELECT * FROM products WHERE ?";
-                connection.query(query, { item_id: answers.itemID }, function (err, res) {
-                    if (err) throw err;
-                    var itemName = res.product_name;
-                    var currentStock = res.quantity;
-                    var updatedStock = parseInt(res.quantity) - itemQTY;
-                    var totalCost = itemQTY * parseFloat(res.price);
-                    console.log(itemName);
-                    console.log(itemName + "  |  " + currentStock + "  |  " + updatedStock + "  |  " + totalCost);
 
-                    if (currentStock >= itemQTY) {
-
-                        connection.query("UPDATE inventory SET ? WHERE ?", [{ item_id: itemID }, { quantity: newStock }], function (err, res) {
-
-                            console.log("For" + itemQTY + " units of " + itemName)
-                            console.log("You have been charged: $" + totalCost);
-                            console.log("Thank you for your purchase!");
-                        });
-                        connection.end();
-                    } else {
-                        console.log("There is not enough inventory for that purchase");
-                        homePage();
+            var query = 'SELECT item_id, product_name, price, quantity FROM products WHERE ?';
+            connection.query(query, { item_id: itemID }, function (err, res) {
+                if (err) throw err;
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].item_id == itemID) {
+                        itemName = res[i].product_name;
+                        currentStock = res[i].quantity;
+                        itemPrice = res[i].price;
                     }
-                })
-            } else {
-                console.log("Sorry, that item is SOLD OUT");
-                homePage();
-            }
+                }
+                var updatedStock = currentStock - itemQTY;
+                var totalCost = itemQTY * parseFloat(itemPrice);
+                // console.log(itemName + "  |  " + currentStock + "  |  " + updatedStock + "  |  " + totalCost);
+
+                if (currentStock >= itemQTY) {
+
+                    connection.query('UPDATE inventory SET ? WHERE ?', [{ item_id: itemID }, { quantity: updatedStock }], function (err, res) {
+
+                        console.log("For " + itemQTY + " units of " + itemName);
+                        console.log("You have been charged: $" + totalCost);
+                        console.log("Thank you for your purchase!");
+                    });
+                    connection.end();
+                } else {
+                    console.log("There is not enough inventory for that purchase");
+                    homePage();
+                }
+            })
         })
 }
